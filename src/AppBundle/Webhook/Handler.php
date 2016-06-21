8f5786dd26591e2aefc4f2d9e7d43d3ffd42b70e
GBProd <contact@gb-prod.fr>
2016-06-21 22:52:07 +0200

    feat: handler for webhook payload (#5)
    * feat: handler for webhook payload
    
    * Scrutinizer Auto-Fixes (#6)
    
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com
    
    * fix: ci

---
+++ src/AppBundle/Webhook/Handler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Webhook;
+
+/**
+ * Webhook handler
+ *
+ * @author gbprod <contact@gb-prod.fr>
+ */
+class Handler
+{
+    /**
+     * Handle webhook payload
+     *
+     * @param array $payload
+     */
+    public function handle(array $payload)
+    {
+
+    }
+}
--- tests/Units/AppBundle/Controller/CallbackControllerTest.php
+++ tests/Units/AppBundle/Controller/CallbackControllerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Units\AppBundle\Controller;
 
 use AppBundle\Controller\CallbackController;
+use AppBundle\Webhook\Handler;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -12,12 +14,47 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class CallbackControllerTest extends \PHPUnit_Framework_TestCase
 {
+    private $handler;
+
+    private $controller;
+
+    public function setUp()
+    {
+        $this->handler = $this->prophesize(Handler::class);
+        $this->controller = new CallbackController(
+            $this->handler->reveal()
+        );
+    }
+
     public function testCallbackReturn204()
     {
-        $controller = new CallbackController();
-        
-        $response = $controller->callback();
-        
+        $request = $this->newRequest('{}');
+
+        $response = $this->controller->callback($request);
+
         $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(204, $response->getStatusCode());
     }
+
+    /**
+     * @param string $content
+     */
+    private function newRequest($content)
+    {
+        return new Request([], [], [], [], [], [], $content);
+    }
+
+    public function testCallbackWillCallHandlerWithUnserializedPayload()
+    {
+        $request = $this->newRequest('{"foo": "bar"}');
+
+        $this
+            ->handler
+            ->handle(['foo' => 'bar'])
+            ->shouldBeCalled()
+        ;
+
+        $this->controller->callback($request);
+    }
+
 }
